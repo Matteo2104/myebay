aa012b7f3af02c40af7c89640bbb6994fe23241e
Matteo2104 <scarcella@outlook.it>
2022-06-14 20:26:45 +0200

    Define and throw a dedicated exception instead of using a generic one

---
+++ src/main/java/it/prova/myebay/exception/AcquistoDAOException.java
@@ -0,0 +1,9 @@
+package it.prova.myebay.exception;
+
+public class AcquistoDAOException extends RuntimeException {
+private static final long serialVersionUID = 1L;
+	
+	public AcquistoDAOException(String message) {
+		super(message);
+	}
+}
