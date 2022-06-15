c8d1dec7b807b9fee15f49190210817803afb4e6
Matteo2104 <scarcella@outlook.it>
2022-06-15 10:09:07 +0200

    modifiche parziali LocalEntityManagerFactoryListener

--- src/main/java/it/prova/myebay/web/listener/LocalEntityManagerFactoryListener.java
+++ src/main/java/it/prova/myebay/web/listener/LocalEntityManagerFactoryListener.java
@@ -1,7 +1,5 @@
 package it.prova.myebay.web.listener;
 
-import java.util.Date;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -10,34 +8,28 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-import it.prova.myebay.model.Categoria;
-import it.prova.myebay.model.Ruolo;
-import it.prova.myebay.model.StatoUtente;
-import it.prova.myebay.model.Utente;
-import it.prova.myebay.service.CategoriaService;
-import it.prova.myebay.service.MyServiceFactory;
-import it.prova.myebay.service.UtenteService;
-
 @WebListener
 public class LocalEntityManagerFactoryListener implements ServletContextListener {
 	
 	private static EntityManagerFactory entityManagerFactory;
 
+	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
 		if (entityManagerFactory != null) {
 			entityManagerFactory.close();
 		}
 	}
 
+	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		try {
+			
 			entityManagerFactory = Persistence.createEntityManagerFactory("myebay_unit");
-			// questa chiamata viene fatta qui per semplicità ma in genere è meglio trovare
-			// altri modi per fare init
-			//initAdminUserAndRuoli();
-		} catch (Throwable ex) {
-			System.err.println("Initial SessionFactory creation failed." + ex);
+			
+		} catch (Exception ex) {
+			
 			throw new ExceptionInInitializerError(ex);
+			
 		}
 	}
 
@@ -56,53 +48,12 @@ public class LocalEntityManagerFactoryListener implements ServletContextListener
 					em.close();
 				}
 			} catch (PersistenceException ex) {
-				System.err.println("Could not close JPA EntityManager" + ex);
-			} catch (Throwable ex) {
-				System.err.println("Unexpected exception on closing JPA EntityManager" + ex);
+				// do nothing
+				return;
+			} catch (Exception ex) {
+				// do nothing
+				return;
 			}
 		}
-	}
-	
-	private void initAdminUserAndRuoli() throws Exception {
-		UtenteService utenteServiceInstance = MyServiceFactory.getUtenteServiceInstance();
-		CategoriaService categoriaServiceInstance = MyServiceFactory.getCategoriaServiceInstance();
-		
-		// utente ADMIN
-		if (utenteServiceInstance.findByUsernameAndPassword("admin", "admin") == null) {
-			Utente admin = new Utente("admin", "admin", "Mario", "Rossi", new Date());
-			admin.setStato(StatoUtente.ATTIVO);
-			admin.setRuolo(Ruolo.ROLE_ADMIN);
-			utenteServiceInstance.inserisciNuovo(admin);	
-		}
-		
-		// utente CLASSIC_USER
-		if (utenteServiceInstance.findByUsernameAndPassword("LesPaul2", "1234") == null) {
-			Utente classic = new Utente("LesPaul2", "1234", "Matteo", "Scarcella", new Date());
-			classic.setStato(StatoUtente.ATTIVO);
-			classic.setRuolo(Ruolo.ROLE_CLASSIC_USER);
-			classic.setCreditoResiduo(300);
-			utenteServiceInstance.inserisciNuovo(classic);	
-		}
-		
-		
-		// categoria VESTITI
-		if (categoriaServiceInstance.findByCodice("VESTITI") == null) {
-			Categoria vestiti = new Categoria("Vestiti", "VESTITI");
-			categoriaServiceInstance.inserisciNuovo(vestiti);	
-		}
-		
-		// categoria TECH
-		if (categoriaServiceInstance.findByCodice("TECH") == null) {
-			Categoria tech = new Categoria("Tech", "TECH");
-			categoriaServiceInstance.inserisciNuovo(tech);
-		}
-		
-		// categoria PER LA CASA
-		if (categoriaServiceInstance.findByCodice("PER LA CASA") == null) {
-			Categoria perLaCasa = new Categoria("Per la casa", "PER LA CASA");
-			categoriaServiceInstance.inserisciNuovo(perLaCasa);
-		}
-		
-		
 	}
 }
